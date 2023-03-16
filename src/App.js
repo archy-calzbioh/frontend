62a958652cc6839938e621771bf399925500dd21
king-archy <king.archy.13013@gmail.com>
2023-03-16 14:59:33 -0700

    merged for deploy

--- src/App.js
+++ src/App.js
@@ -7,6 +7,54 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 const App = () => {
+  const [tasks, setTasks] = useState([])
+  const [showTask, setShowTask] = useState({})
+
+  const getTasks = () => {
+    axios.get('https://taskawaybackend.onrender.com/ta/tasks').then(response => {
+      setTasks(response.data)
+    })
+  }
+
+  const handleCreate = (data) => {
+    axios.post(`https://taskawaybackend.onrender.com/ta/tasks`, data).then(response => {
+      setTasks([...tasks, response.data])
+    })
+  }
+
+  const handleDelete = (id) => {
+    axios.delete(`https://taskawaybackend.onrender.com/ta/tasks/${id}`).then(response => {
+      setTasks(tasks.filter(task => {
+        return task._id !== response.data._id
+      }))
+
+    })
+  }
+
+  const handleEdit = (data) => {
+    console.log(data)
+    document.getElementById(`${data._id + 1}`).classList.remove('hidden')
+    axios.put(`https://taskawaybackend.onrender.com/ta/tasks/${data._id}`, data).then(response => {
+      setTasks(tasks.map(task => {
+        return task._id !== data._id ? task : data
+      }))
+
+    })
+  }
+
+  const handleShowTask = (data) => {
+    axios.get(`https://taskawaybackend.onrender.com/ta/tasks/${data._id}`).then(response => {
+      setShowTask(response.data)
+    })
+
+  }
+
+
+
+  useEffect(() => {
+    getTasks()
+  }, [])
+
   return (
     <div className="App">
       <BrowserRouter>
